1c503114e0775a8b6612c92b5334cc2606836fb0
MrKitty7 <107936754+MrKitty7@users.noreply.github.com>
2022-08-07 00:22:52 +0200

    added git ignore, updated code, to be continued

--- wallet.py
+++ wallet.py
@@ -27,9 +27,38 @@ def edit_balance():
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
     print('---------------')
-    
 
-    cursor.execute()
+    current_bal_hrk = cursor.execute('SELECT hrk FROM balance').fetchone()[0]
+    current_bal_eur = cursor.execute('SELECT eur FROM balance').fetchone()[0]
+    current_bal_usd = cursor.execute('SELECT usd FROM balance').fetchone()[0]
+    current_bal_gbp = cursor.execute('SELECT gbp FROM balance').fetchone()[0]
+    
+    print('Your current balance is: ')
+    print(f'HRK: {current_bal_hrk}')
+    print(f'EUR: {current_bal_eur}')
+    print(f'USD: {current_bal_usd}')
+    print(f'GBP: {current_bal_gbp}')
+    
+    print('---------------')
+    new_raw_bal = int(input('Enter your new balance. '))
+    new_raw_bal_cur = input('Enter the currency your balance is in. ')
+
+    currencies = ['HRK', 'EUR', 'USD', 'GBP']
+
+    for currency in currencies:
+        if currency.lower() == new_raw_bal_cur.lower():
+            current_currency = currency
+            currencies.remove(currency)
+
+    hrk = cursor.execute('SELECT hrk FROM currencies').fetchone()[0]
+    eur = cursor.execute('SELECT eur FROM currencies').fetchone()[0]
+    usd = cursor.execute('SELECT usd FROM currencies').fetchone()[0]
+    gbp = cursor.execute('SELECT gbp FROM currencies').fetchone()[0]
+
+
+
+    for currency in currencies:
+        pass
 
 
     connection.commit()
@@ -39,10 +68,10 @@ def list_currencies():
     connection = sqlite3.connect("database.db")
     cursor = connection.cursor()
 
-    hrk = cursor.execute('SELECT hrk FROM currencies').fetchall()
-    eur = cursor.execute('SELECT eur FROM currencies').fetchall()
-    usd = cursor.execute('SELECT usd FROM currencies').fetchall()
-    gbp = cursor.execute('SELECT gbp FROM currencies').fetchall()
+    hrk = cursor.execute('SELECT hrk FROM currencies').fetchone()[0]
+    eur = cursor.execute('SELECT eur FROM currencies').fetchone()[0]
+    usd = cursor.execute('SELECT usd FROM currencies').fetchone()[0]
+    gbp = cursor.execute('SELECT gbp FROM currencies').fetchone()[0]
 
     print(f'HRK: {hrk}, EUR: {eur}, USD: {usd}, GBP: {gbp}')
 
